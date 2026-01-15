cd9beea5f7b970ed1b80b0d01ea0d8f0febffa5e
EveningSt3r <daawesomeboi@gmail.com>
2026-01-14 23:10:05 -0800

    Added hello world to embedded folder

---
+++ embedded/sketch_jan12a.ino
@@ -0,0 +1,13 @@
+
+// Samuel You Hello World
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Hello world!");
+  delay(1000);
+
+
+}
+
+void loop() {
+}
